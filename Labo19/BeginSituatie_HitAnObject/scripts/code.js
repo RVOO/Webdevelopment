a9808a9c4ef51d904650ce7a0b761fdc2e84ab82
RVOO <ruben.vanoostende@student.vives.be>
2024-04-01 22:26:48 +0200

    Labo 19

--- Labo19/BeginSituatie_HitAnObject/scripts/code.js
+++ Labo19/BeginSituatie_HitAnObject/scripts/code.js
@@ -1,12 +1,136 @@
-
-
-
+let global = {
+    IMAGE_COUNT: 5, // aantal figuren
+    IMAGE_SIZE: 48, // grootte van de figuur
+    IMAGE_PATH_PREFIX: "images/", // map van de figuren
+    IMAGE_PATH_SUFFIX: ".png", // extensie van de figuren
+    MOVE_DELAY: 3000, // aantal milliseconden voor een nieuwe afbeelding verschijnt
+    score: 0, // aantal hits
+    timeoutId: 0 // id van de timeout timer, zodat we die kunnen annuleren
+};
 const setup = () => {
-
+    let imgs = document.getElementsByClassName("target");
+    let startBtn = document.getElementById("startBtn");
+    for(let i = 0; i<imgs.length; i++){
+        imgs[i].classList.add("hidden");
+    }
+    startBtn.addEventListener("click", play);
+    global.timeoutId = setInterval(randomImg, 2000);
 };
 
+const play = (event) =>{
+    let btn = event.currentTarget;
+    btn.classList.add("hidden");
+    score = 0;
+    let points = document.getElementById("score");
+    points.innerHTML = "Aantal hits: " + score;
 
+    let add = document.getElementById("addBtn");
+    let del = document.getElementById("delBtn");
+    add.addEventListener("click", addTarget);
+    del.addEventListener("click", delTarget);
 
+    let targets = document.getElementsByClassName("target");
+    for(let i = 0; i<targets.length; i++){
+        targets[i].classList.remove("hidden");
+        targets[i].removeEventListener("click", lost);
+    }
+    randomNoClick();
+}
+
+const lost = () =>{
+    window.alert("Game over!");
+    clearTimeout(global.timeoutId);
+    let btn = document.getElementById("startBtn");
+    btn.classList.remove("hidden");
+    btn.value = "Play again";
+}
+
+const switchPlacesClick = (event) =>{
+    let img = event.currentTarget;
+    let height = img.height/2 + Math.random()  * (img.parentElement.scrollHeight - img.height*2);
+    let width = img.width/2 + Math.random() * (img.parentElement.scrollWidth - img.width*2);
+    let points = document.getElementById("score");
+    score++;
+    points.innerHTML = "Aantal hits: " + score;
+    img.style.top = height.toString() + "px";
+    img.style.left = width.toString() + "px";
+}
+
+const randomImgClick = (event)=>{
+    let img = event.currentTarget;
+    let random = Math.floor(Math.random() * global.IMAGE_COUNT);
+    img.setAttribute("src", `${global.IMAGE_PATH_PREFIX}${random}${global.IMAGE_PATH_SUFFIX}`);
+
+    if(random === 0) {
+        img.removeEventListener("click", randomImgClick);
+        img.removeEventListener("click", switchPlacesClick);
+        img.addEventListener("click", lost);
+    }else{
+        img.removeEventListener("click", lost);
+        img.addEventListener("click", randomImgClick);
+        img.addEventListener("click", switchPlacesClick);
+    }
+}
+
+const randomNoClick = () =>{
+    let targets = document.getElementsByClassName("target");
+    for(let i = 0; i<targets.length; i++){
+        let random = Math.floor(Math.random() * global.IMAGE_COUNT);
+        targets[i].setAttribute("src", `${global.IMAGE_PATH_PREFIX}${Math.floor(random)}${global.IMAGE_PATH_SUFFIX}`);
+
+        if(random === 0) {
+            targets[i].removeEventListener("click", randomImgClick);
+            targets[i].removeEventListener("click", switchPlacesClick);
+            targets[i].addEventListener("click", lost);
+        }else{
+            targets[i].removeEventListener("click", lost);
+            targets[i].addEventListener("click", randomImgClick);
+            targets[i].addEventListener("click", switchPlacesClick);
+        }
+
+        let height = targets[i].height/2 + Math.random()  * (targets[i].parentElement.scrollHeight - targets[i].height*2);
+        let width = targets[i].width/2 + Math.random() * (targets[i].parentElement.scrollWidth - targets[i].width*2);
+        targets[i].style.top = height.toString() + "px";
+        targets[i].style.left = width.toString() + "px";
+    }
+}
+
+const randomImg = ()=>{
+    let targets = document.getElementsByClassName("target");
+    for(let i = 0; i<targets.length; i++) {
+       let random =Math.floor(Math.random() * global.IMAGE_COUNT);
+        targets[i].setAttribute("src", `${global.IMAGE_PATH_PREFIX}${random}${global.IMAGE_PATH_SUFFIX}`);
+
+        if (random === 0) {
+            targets[i].removeEventListener("click", randomImgClick);
+            targets[i].removeEventListener("click", switchPlacesClick);
+            targets[i].addEventListener("click", lost);
+        } else {
+            targets[i].removeEventListener("click", lost);
+            targets[i].addEventListener("click", randomImgClick);
+            targets[i].addEventListener("click", switchPlacesClick);
+        }
+    }
+}
+
+const addTarget = () =>{
+    let target = document.createElement("img");
+    target.setAttribute("src","images/0.png");
+    target.setAttribute("alt", "target");
+    target.classList.add("target");
+    let playField = document.getElementById("playField");
+    playField.appendChild(target);
+    randomNoClick();
+}
+
+const delTarget = () =>{
+    let targets = document.getElementsByClassName("target");
+    if(targets.length !== 1){
+        targets[targets.length - 1].remove();
+    }else{
+        window.alert("At least one survivor must remain!");
+    }
+}
 
 window.addEventListener("load", setup);
 
---
+++ Labo19/Matching game/scripts/index.js
@@ -0,0 +1,226 @@
+let global = {
+    AANTAL_HORIZONTAAL : 4,
+    AANTAL_VERTICAAL : 3,
+    aantalPhotos : [],
+    TOTAL_AMOUNT_OF_CARDS : 0,
+    POSSIBLE_SUBJECTS : [
+        "Penguins",
+        "Groot",
+        "Seals"
+    ],
+    PHOTOS_PER_POSSIBLE_SUBJECT : [
+        6,
+        7,
+        6
+    ],
+    subject: [],
+    PIC_PATH : "images/",
+    PIC_TYPE: ".jpg",
+    BACKGROUND_PATH : "images/background.jpg",
+    AMOUNT_TO_MATCH : 2,
+    tries : 0
+}
+
+const setup = () => {
+    addSubjectChooser();
+    const playBtn = document.getElementById("startBtn");
+    playBtn.addEventListener("click", play);
+    const restartBtn = document.getElementById("restart");
+    restartBtn.addEventListener("click", restart);
+    const inputs = document.getElementsByClassName("subjectInput");
+    for(let i = 0; i<inputs.length; i++){
+        subjectCheck(inputs[i])
+    }
+    const amountOfCards = document.getElementById("amountOfCards");
+    updateAmount();
+    amountOfCards.addEventListener("click", updateAmount);
+    amountOfCards.addEventListener("input", updateAmount);
+}
+
+const updateAmount = () =>{
+    const amountOfCards = document.getElementById("amountOfCards");
+    global.AMOUNT_TO_MATCH = Number(amountOfCards.value);
+}
+
+const addSubjectChooser = () =>{
+    const parent = document.getElementById("Subjects");
+    let subjects = clearNullsList(global.POSSIBLE_SUBJECTS);
+    let cardCount = clearNullsList(global.PHOTOS_PER_POSSIBLE_SUBJECT);
+    for(let i = 0; i<subjects.length; i++){
+        let label = document.createElement("label");
+        let input = document.createElement("input");
+        label.innerHTML = `${subjects[i]}:`;
+        label.setAttribute("for", `${subjects[i]}`);
+        input.setAttribute("Data-subject", `${subjects[i]}`);
+        input.setAttribute("type", "number");
+        input.setAttribute("id", `${subjects[i]}`);
+        input.setAttribute("name", `Cardcount of ${subjects[i]}`);
+        input.setAttribute("max", `${cardCount[i]}`);
+        input.setAttribute("min", "0");
+        input.setAttribute("value", `${cardCount[i]/2}`);
+        label.classList.add("inputSubject");
+        input.classList.add("subjectInput");
+        input.addEventListener("click", subjectUpdate);
+        input.addEventListener("input", subjectUpdate);
+        parent.appendChild(label);
+        parent.appendChild(input);
+    }
+}
+
+const subjectUpdate = (event)=>{
+    let input = event.currentTarget;
+    subjectCheck(input);
+}
+const subjectCheck = (input) =>{
+    if(input.value === "0" || input.value === null){
+        let index = global.subject.indexOf(input.getAttribute("Data-subject"));
+        global.subject[index] = null;
+        global.aantalPhotos[index] = null;
+    }
+    else if(global.subject.indexOf(input.getAttribute("Data-subject")) === -1) {
+        global.subject.push(input.getAttribute("Data-subject"));
+        global.aantalPhotos.push(input.value);
+    }else{
+        global.aantalPhotos[global.subject.indexOf(input.getAttribute("Data-subject"))] = input.value;
+    }
+}
+
+const play = () =>{
+    if(clearNullsList(global.aantalPhotos).length !== 0) {
+        const begin = document.getElementById("Beginning");
+        begin.classList.add("hidden");
+        createCards();
+        const gameField = document.getElementById("Game");
+        gameField.classList.remove("hidden");
+        amountOfCards();
+    }else{
+        window.alert("No subject chosen");
+    }
+}
+
+const createListCardPaths = () =>{
+    const pics = [];
+    for(let i = 0; i< global.AMOUNT_TO_MATCH; i++) {
+        for (let i = 0; i < global.aantalPhotos.length; i++) {
+            let amount = global.aantalPhotos[i];
+            let subject = global.subject[i];
+            for (let ind = 0; ind < amount; ind++) {
+                pics.push(`${global.PIC_PATH}${subject}_${ind}${global.PIC_TYPE}`);
+            }
+        }
+    }
+    return pics;
+}
+
+const createCards = () =>{
+    const playField = document.getElementById("PlayField");
+    let pics = createListCardPaths();
+    while(pics.length > 0) {
+        const img = document.createElement("img");
+        img.classList.add("card");
+        img.setAttribute("src", global.BACKGROUND_PATH);
+        let random = Math.floor(Math.random()*pics.length);
+        img.setAttribute("Data-pic", pics[random]);
+        pics[random] = null;
+        pics = clearNullsList(pics);
+        img.addEventListener("click", turn);
+        img.addEventListener("click", update);
+        playField.appendChild(img);
+    }
+}
+
+const turn = (event) =>{
+    let amountTurned = document.getElementsByClassName("turned").length;
+    if(amountTurned < global.AMOUNT_TO_MATCH) {
+        let img = event.currentTarget;
+        img.setAttribute("src", img.getAttribute("Data-pic"));
+        img.classList.add("turned");
+        let audio = new Audio("sounds/Card_Flip.mp3");
+        audio.play();
+    }
+}
+
+const update = () =>{
+    const turned = document.getElementsByClassName("turned");
+    let length = turned.length;
+    if(length === global.AMOUNT_TO_MATCH){
+        global.tries++;
+        const tries = document.getElementById("triesH");
+        tries.innerText = global.tries;
+        let match = true;
+        for(let i = 0; i<length && match === true; i++){
+            if(turned[i].getAttribute("src") !== turned[0].getAttribute("src")){
+                match = false;
+            }
+        }
+        if(match === true){
+            let audio = new Audio("sounds/Ding_Correct.mp3");
+            audio.play();
+            for(let i = 0; i<length; i++){
+                turned[0].classList.add("found");
+                turned[0].removeEventListener("click", turn);
+                turned[0].removeEventListener("click", update);
+                turned[0].classList.remove("turned");
+            }
+
+        }else{
+            let audio = new Audio("sounds/Sad_Trombone.mp3");
+            audio.play();
+            setTimeout(turnBack, 500);
+        }
+    }
+
+    const found = document.getElementsByClassName("found");
+    if(found.length === global.TOTAL_AMOUNT_OF_CARDS){
+        endGame();
+    }
+}
+
+const amountOfCards = () =>{
+    let amount = 0;
+    for(let i = 0; i<global.aantalPhotos.length; i++){
+        amount += Number(global.aantalPhotos[i]*global.AMOUNT_TO_MATCH);
+    }
+    global.TOTAL_AMOUNT_OF_CARDS = amount;
+}
+
+const turnBack = () =>{
+    const turned = document.getElementsByClassName("turned");
+    let length = turned.length;
+    for(let i = 0; i<length; i++){
+        turned[0].setAttribute("src", global.BACKGROUND_PATH);
+        turned[0].classList.remove("turned");
+    }
+}
+
+const clearNullsList = (list) =>{
+    const resultList = [];
+    for(let i = 0; i<list.length; i++){
+        if(list[i] !== null && list[i] !== ""){
+            resultList.push(list[i]);
+        }
+    }
+    return resultList;
+}
+
+const endGame = () =>{
+    const gameField = document.getElementById("Game");
+    gameField.classList.add("hidden");
+    const div = document.getElementById("Ending");
+    div.classList.remove("hidden");
+    const tries = document.getElementById("tries");
+    tries.innerText = global.tries;
+}
+
+const restart = () =>{
+    global.tries = 0;
+    const tries = document.getElementById("triesH");
+    tries.innerText = global.tries;
+    const end = document.getElementById("Ending");
+    end.classList.add("hidden");
+    const begin = document.getElementById("Beginning");
+    begin.classList.remove("hidden");
+    const playField = document.getElementById("PlayField");
+    playField.innerHTML = "";
+}
+window.addEventListener("load", setup);
